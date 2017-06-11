4b221a88d1d3b4d71c4d103422598957099bd73a
Gustavo Dias <gust.webdev@gmail.com>
2017-06-11 05:17:06 -0300

    Adicionado exemplo de output com formação

---
+++ golang-02-formated_output/binary-decimal.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Decimal: %d - Binário: %b", 23, 23)
+	//Printf resulta em um output com formação. Geralmente feita através do sinão de porcentagem
+}
