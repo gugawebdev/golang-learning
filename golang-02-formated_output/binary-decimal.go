33baa2af3d91b4b3e498de2837fbcf8e350eb161
Gustavo Dias <gust.webdev@gmail.com>
2017-06-11 19:03:03 -0300

    Exericios de estrutura de repetição 'FOR'

--- golang-02-formated_output/binary-decimal.go
+++ golang-02-formated_output/binary-decimal.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Decimal: %d - Binário: %b", 23, 23)
+	fmt.Printf("Decimal: %d\nBinário: %b", 23, 23)
 	//Printf resulta em um output com formação. Geralmente feita através do sinão de porcentagem
 }
---
+++ golang-03-for_loop/for-loop.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 100; i++ {
+		fmt.Printf("Decimal: %d \t Binário: %b \t \n", i, i)
+	}
+}
